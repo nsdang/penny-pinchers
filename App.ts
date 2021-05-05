c9fc8a6dbbe841949bdbb7670bc4c5088ebdf5bb
My Tran <mytran1109@gmail.com>
2021-05-04 17:27:22 -0700

    delete all comments in App.ts

--- App.ts
+++ App.ts
@@ -7,17 +7,12 @@ import * as bodyParser from "body-parser";
 //var MongoClient = require('mongodb').MongoClient;
 //var Q = require('q');
 
-//import {ListModel} from './model/ListModel';
-//import {TaskModel} from './model/TaskModel';
-//import {DataAccess} from './DataAccess';
 import { SubListModel } from "./model/SubListModel";
 
 // Creates and configures an ExpressJS web server.
 class App {
   // ref to Express instance
   public expressApp: express.Application;
-  // public Lists:ListModel;
-  // public Tasks:TaskModel;
   public SubscriptionList: SubListModel;
   public idGenerator: number;
 
@@ -40,10 +35,6 @@ class App {
   // Configure API endpoints.
   private routes(): void {
     let router = express.Router();
-
-    /******************************************************
-     * ***************Penny-pinchers' Router*******************
-     ******************************************************/
 
     // get all items
     router.get("/app/list/:listId", (req, res) => {
@@ -82,41 +73,7 @@ class App {
       res.send("PUT: /app/items/:itemId");
     });
 
-    // router.get("/app/list/:listId/count", (req, res) => {
-    //   var id = req.params.listId;
-    //   console.log("Query single list with id: " + id);
-    //   // this.Tasks.retrieveTasksCount(res, {listId: id});
-    // });
-
-    // router.post("/app/list/", (req, res) => {
-    //   console.log(req.body);
-    //   var jsonObj = req.body;
-    //   //jsonObj.listId = this.idGenerator;
-    //   /* this.Lists.model.create([jsonObj], (err) => {
-    //         if (err) {
-    //             console.log('object creation failed');
-    //         }
-    //     });
-    //     */
-    //   res.send(this.idGenerator.toString());
-    //   this.idGenerator++;
-    // });
-
-    // router.get("/app/list/:listId", (req, res) => {
-    //   var id = req.params.listId;
-    //   console.log("Query single list with id: " + id);
-    //   //this.Tasks.retrieveTasksDetails(res, {listId: id});
-    // });
-
-    // router.get("/app/list/", (req, res) => {
-    //   console.log("Query All list");
-    //   //this.Lists.retrieveAllLists(res);
-    // });
-
-    // router.get("/app/listcount", (req, res) => {
-    //   console.log("Query the number of list elements in db");
-    //   // this.Lists.retrieveListCount(res);
-    // });
+    
 
     this.expressApp.use("/", router);
 
