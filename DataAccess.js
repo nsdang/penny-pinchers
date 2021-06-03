27095bb8e8ddd0f0fb825a6eec17f93c3a0a7891
cangelica <cangelica1@seattleu.edu>
2021-06-02 21:39:18 -0700

    Changed connection string in DataAccess.ts

--- DataAccess.js
+++ DataAccess.js
@@ -3,6 +3,7 @@ exports.__esModule = true;
 exports.DataAccess = void 0;
 var Mongoose = require("mongoose");
 var DataAccess = /** @class */ (function () {
+    //static DB_CONNECTION_STRING:string = 'mongodb://dbAdmin:test@localhost:3000/pennyPinchers?authSource=admin';
     function DataAccess() {
         DataAccess.connect();
     }
@@ -16,8 +17,7 @@ var DataAccess = /** @class */ (function () {
         this.mongooseInstance = Mongoose.connect(this.DB_CONNECTION_STRING);
         return this.mongooseInstance;
     };
-    //static DB_CONNECTION_STRING:string = 'mongodb+srv://pinchers:KXy35VTdZ7ak0v1M@penny-pinchers1.jss5q.mongodb.net/test1?retryWrites=true&w=majority';
-    DataAccess.DB_CONNECTION_STRING = 'mongodb://dbAdmin:test@localhost:3000/pennyPinchers?authSource=admin';
+    DataAccess.DB_CONNECTION_STRING = 'mongodb+srv://pinchers:2SLEsdZPN38rbT8@penny-pinchers1.jss5q.mongodb.net/ppDB?retryWrites=true&w=majority';
     return DataAccess;
 }());
 exports.DataAccess = DataAccess;
