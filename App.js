2efbd163e12d20ecc7dc148ca4e7b359005108cd
cangelica <cangelica1@seattleu.edu>
2021-06-02 22:57:20 -0700

    SAD

--- App.js
+++ App.js
@@ -167,7 +167,7 @@ var App = /** @class */ (function () {
         this.expressApp.use("/", router);
         this.expressApp.use("/app/json/", express.static(__dirname + "/app/json"));
         this.expressApp.use("/images", express.static(__dirname + "/img"));
-        this.expressApp.use("/", express.static(__dirname + "/angular-dist/dist/app"));
+        this.expressApp.use("/", express.static(__dirname + "/angular-dist/dist/"));
     };
     return App;
 }());
