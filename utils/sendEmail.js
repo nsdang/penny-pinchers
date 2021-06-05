90e45519dea5453562a4d113f49bbb51dd2e2767
Dang Nguyen <dangns229@gmail.com>
2021-06-05 12:12:39 -0700

    Add email template

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,7 +1,29 @@
+var fs = require("fs");
+
+// const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+// // with a + sign before the phone number
+// const phonenoPlus = /^\+?[1-9]\d{1,14}$/;
+
+// const validationErrors = {};
+// if (shareMethod == "Email") {
+//   if (!userInfo.email.match(mailformat)) {
+//     validationErrors["email"] = "This is not a valid email address";
+//   }
+// } else {
+//   if (!userInfo.phoneNumber.match(phonenoPlus)) {
+//     validationErrors["phoneno"] = "This is not a valid phone number";
+//   }
+// }
+// if (Object.keys(validationErrors).length > 0) {
+//   throw new UserInputError("Failed to share due to validation errors", { validationErrors });
+// }
+
 const sendEmail = async ({ subscription, client }) => {
   const sgMail = require("@sendgrid/mail");
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  const msg = {
+  console.log(process.env.SENDGRID_API_KEY);
+  var msg = {
     to: client.email, // Change to your recipient
     from: { email: "dnguyen1@seattleu.edu", name: "Penny Pinchers" }, // Change to your verified sender
     subject: `Reminder for upcoming ${subscription.serviceName} pay day!!!`,
@@ -13,14 +35,34 @@ const sendEmail = async ({ subscription, client }) => {
     <h3> Make sure to unsubscribe berfore the due date!</h3>
     </div>`,
   };
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log("Email sent");
-    })
-    .catch((error) => {
-      console.error(error.response.body.errors);
-    });
+
+  fs.readFile("./utils/email_template.html", function (err, html) {
+    if (err) {
+      throw error;
+    }
+    msg.html = html.toString();
+
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log("Email sent");
+      })
+      .catch((error) => {
+        console.error(error.body);
+      });
+  });
+};
+
+const recurCheckPayDay = (list, item, user ) => {
+  function checkDueDateEvery24Hours(i) {
+    setTimeout(() => {
+      console.log("Infinite Loop Test n:", i);
+      checkDueDateEvery24Hours(++i);
+    }, 1000 * 60 * 60 * 24);
+  }
+
+  checkDueDateEvery24Hours(0);
 };
 
 module.exports.sendEmail = sendEmail;
+module.exports.recurCheckPayDay = recurCheckPayDay;
